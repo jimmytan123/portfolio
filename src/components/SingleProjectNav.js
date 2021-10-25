5ae863ce4ffb57580b8a3ba798938793437ec18b
Jimmy Tan <jimmytan0424@gmail.com>
2021-10-25 10:06:38 -0700

    change styling for nav button in single project page

--- src/components/SingleProjectNav.js
+++ src/components/SingleProjectNav.js
@@ -1,19 +1,19 @@
 import { Link } from 'react-router-dom';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import { BiChevronRight, BiChevronLeft } from 'react-icons/bi';
 
 const SingleProjectNav = ({ nextProjectId }) => {
   return (
     <section className="single-project-nav">
       <Link to={'/work'}>
         <span>
-          <FaAngleLeft />
+          <BiChevronLeft />
         </span>
         Return Work
       </Link>
       <Link to={`/project-${nextProjectId}`}>
         Next Project{' '}
         <span>
-          <FaAngleRight />
+          <BiChevronRight />
         </span>
       </Link>
     </section>
